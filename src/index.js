6da856a0c44e39d5e54623ca4846109ee0cdbfb4
khalil <khalilhassebi@gmail.com>
2019-01-19 20:06:08 +0100

    Retrieve one repo with json

--- src/index.js
+++ src/index.js
@@ -1,37 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import $ from 'jquery';
 class Repo extends React.Component
 {
   render()
   {
       return(
-        <div class="repo">
-          <div class="repo-image">
-            <img src=""/>
-          </div>
-          <div class="repo-info">
-            <ul>
-              <li><h4 class="repo-name">Repo Name:</h4></li>
-              <li><p class="repo-desc">Repository Description</p></li>
-              <li>
-                <div class="repo-stat">
-                  <ul>
-                    <li><p>NB Stars</p></li>
-                    <li><p>Issues</p></li>
-                    <li><p>Time interval by Owner Name</p></li>
-                  </ul>
+        <div className="container">
+          <div className="repo">
+            <div className="repo-image">
+              <img src={this.props.repoImg} alt={this.props.repoOwner}/>
+            </div>
+            <div className="repo-info">
+              <ul>
+                <li><h4 className="repo-name">{this.props.repoName}</h4></li>
+                <li><p className="repo-desc">{this.props.repoDesc}</p></li>
+                <li>
+                  <div className="repo-stat">
+                    <ul>
+                      <li><p>{this.props.repoStars}</p></li>
+                      <li><p>{this.props.repoIssues}</p></li>
+                      <li><p>{this.props.repoTimeInter} by {this.props.repoOwner}</p></li>
+                    </ul>
 
-                </div>
-              </li>
-            </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       );
   }
 }
-ReactDOM.render(
-  <Repo />,
-  document.getElementById('root')
-)
+//var date = new Date();
+// var githubDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+// var githubLink = "https://api.github.com/search/repositories?q=created:>"+ githubDate +"&sort=stars&order=desc";
+var githubLink2 = "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+let name,desc,stars,issues,login,avatar;
+$.getJSON(githubLink2,function(json)
+ {
+   name=json.items[0].name;
+   desc = json.items[0].description;
+   stars = json.items[0].stargazers_count;
+   issues = json.items[0].open_issues_count;
+   login = json.items[0].owner.login;
+   avatar = json.items[0].owner.avatar_url;
+   console.log(name);
+   ReactDOM.render(
+     <Repo repoImg={avatar} repoName={name} repoDesc={desc} repoStars={stars} repoIssues={issues} repoTimeInter="39 day ago" repoOwner={login}/>,
+     document.getElementById('root')
+   )
+ });
