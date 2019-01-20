065135e88cb0ff6ee4537d57101f44abf457a07e
khalil <khalilhassebi@gmail.com>
2019-01-20 19:15:24 +0100

    Get Result of first Page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import $ from 'jquery';
+// class for to define repos structure
 class Repo extends React.Component
 {
   render()
@@ -19,9 +20,9 @@ class Repo extends React.Component
                 <li>
                   <div className="repo-stat">
                     <ul>
-                      <li><p>{this.props.repoStars}</p></li>
-                      <li><p>{this.props.repoIssues}</p></li>
-                      <li><p>{this.props.repoTimeInter} by {this.props.repoOwner}</p></li>
+                      <li><p><i class="fas fa-star"></i> {this.props.repoStars}</p></li>
+                      <li><p><i class="fas fa-exclamation-circle"></i> {this.props.repoIssues}</p></li>
+                      <li><p>{this.props.repoTimeInter} by <i class="fas fa-user"></i> {this.props.repoOwner}</p></li>
                     </ul>
 
                   </div>
@@ -38,17 +39,31 @@ class Repo extends React.Component
 // var githubLink = "https://api.github.com/search/repositories?q=created:>"+ githubDate +"&sort=stars&order=desc";
 var githubLink2 = "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
 let name,desc,stars,issues,login,avatar;
+var indents = [];
 $.getJSON(githubLink2,function(json)
  {
-   name=json.items[0].name;
-   desc = json.items[0].description;
-   stars = json.items[0].stargazers_count;
-   issues = json.items[0].open_issues_count;
-   login = json.items[0].owner.login;
-   avatar = json.items[0].owner.avatar_url;
-   console.log(name);
+   for(var i = 0; i < json.items.length;i++)
+   {
+     // limiting the maximum number of name charac to 53
+     name= json.items[i].name;
+     if(name.length > 53)
+     {
+       name = name.substr(0,53) + "...";
+     }
+     // limiting the maximum number of desc charac to 173
+     desc = json.items[i].description;
+     if(desc.length > 173)
+     {
+       desc = desc.substr(0,173) + "...";
+     }
+     stars = json.items[i].stargazers_count;
+     issues = json.items[i].open_issues_count;
+     login = json.items[i].owner.login;
+     avatar = json.items[i].owner.avatar_url;
+     indents.push(<Repo repoImg={avatar} repoName={name} repoDesc={desc} repoStars={stars} repoIssues={issues} repoTimeInter="39 day ago" repoOwner={login}/>);
+   }
    ReactDOM.render(
-     <Repo repoImg={avatar} repoName={name} repoDesc={desc} repoStars={stars} repoIssues={issues} repoTimeInter="39 day ago" repoOwner={login}/>,
+     indents,
      document.getElementById('root')
    )
  });
